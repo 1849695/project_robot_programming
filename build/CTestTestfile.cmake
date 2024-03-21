0e1552190e818cf04ca3bcb5854cdf522c02eeb3
1849695 <recchi.1849695@studenti.uniroma1.it>
2024-03-21 17:29:37 +0100

    prima commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/lattinone/Desktop/project_robot_programming/src
+# Build directory: /home/lattinone/Desktop/project_robot_programming/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ros_pkg")
