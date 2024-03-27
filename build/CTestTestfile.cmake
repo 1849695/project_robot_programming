0e62fb0a732c5595dba85b41434a313191dbb005
1849695 <recchi.1849695@studenti.uniroma1.it>
2024-03-27 11:45:21 +0100

    change program in order to fix some eror

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lattinone/Desktop/project_robot_programming/src
-# Build directory: /home/lattinone/Desktop/project_robot_programming/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("ros_pkg")
--- build/ros_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lattinone/Desktop/project_robot_programming/src/ros_pkg
-# Build directory: /home/lattinone/Desktop/project_robot_programming/build/ros_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
